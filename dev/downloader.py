f2ce6b67e2bd7f2ed2e96c4b6153b4e0ba44a179
Fernando Seiti Furusato <fernando.furusato@lnls.br>
2019-06-15 17:57:12 -0300

    Added download progress
    Signed-off-by: Fernando Seiti Furusato <fernando.furusato@lnls.br>

--- dev/downloader.py
+++ dev/downloader.py
@@ -3,6 +3,7 @@ import h5py
 
 def download_hdf5(file_id, file_name=None):
     
+    downloaded = 0.0
     try:
         article_url = 'https://api.figshare.com/v2/articles/8206559/files/{}'.format(file_id)
         article_response = requests.get(article_url)
@@ -14,20 +15,27 @@ def download_hdf5(file_id, file_name=None):
         elif not file_name:
             file_name = 'generic.h5'
         
+        size_in_bytes = float(article_response.json()['size'])
+        
         print('File name: {}\nFile size: {:.2f}MB'.format(name, float(article_response.json()['size'])/(1024**2)))
         
         url = 'https://api.figshare.com/v2/file/download/{}'.format(file_id)
         
-        print('Downloading file...')
+        print('Locating file...')
         response = requests.get(url, stream=True)
         print('Done!')
-    except as e:
+    except Exception as e:
         print('Error while trying to download file: {}'.format(e))
     try:
-        print('Saving file as {}'.format(file_name))
+        print('Downloading file to {}'.format(file_name))
         with open(file_name, 'wb') as fd:
-            for chunk in response.iter_content(chunk_size=128):
+            for chunk in response.iter_content(chunk_size=1024**2):
+                downloaded += 1024**2
                 fd.write(chunk)
-        print('Done!')
-    except as e:
+                percent = (downloaded * 100) / size_in_bytes
+                if percent > 100:
+                    percent = 100
+                print('{:.2f}%'.format(percent), end='\r')
+        print('\nDone!')
+    except Exception as e:
         print('Error while trying to write file: {}'.format(e))
